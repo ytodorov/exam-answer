40d0fd7e6cd54931150bf1197ca291d8f6bba546
ytodorov <ytodorov@ytodorov.com>
2019-03-08 23:15:14 +0200

    initial commit

---
+++ Exam-answer/Exam-answerWeb/Controllers/Az100Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exam_AnswerWeb.Controllers
+{   
+    [Route("microsoft/az-100/implement-and-manage-storage")]
+    public class MicrosoftController : Controller
+    {
+        private const string folderName = "az-100/implement-and-manage-storage";
+
+        [Route("question1")]
+        public IActionResult Question1()
+        {
+            return View($"{folderName}/question1");
+        }
+
+        [Route("question2")]
+        public IActionResult Question2()
+        {
+            return View($"{folderName}/question2");
+        }
+    }
+}
---
+++ Exam-answer/Exam-answerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exam_answerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
