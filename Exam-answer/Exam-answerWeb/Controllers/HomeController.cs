f6c7b96ed1797224eb1ad0e6a9b3ef20c595f912
ytodorov <ytodorov@ytodorov.com>
2019-03-21 23:22:33 +0200

    Cert

--- Exam-answer/Exam-answerWeb/Controllers/HomeController.cs
+++ Exam-answer/Exam-answerWeb/Controllers/HomeController.cs
@@ -25,5 +25,11 @@ namespace Exam_answerWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [Route("/.well-known/pki-validation/D67C33543D08AB8D948517B8C6FC8AB6.txt")]
+        public string Cert()
+        {
+            return "C4EFB5C501298A5174718DC052BC2C7EBFF2038F2883504865F5B1A617013EA5 comodoca.com 5c93d4a8e2e04";
+        }
     }
 }
