f8947d11c4213e7edc010460704c27d2001f4550
ytodorov <ytodorov@ytodorov.com>
2019-04-18 09:28:54 +0300

    30 icnl txt

--- Exam-answer/Exam-answerWeb/Controllers/SalesforceController.cs
+++ Exam-answer/Exam-answerWeb/Controllers/SalesforceController.cs
@@ -53,7 +53,7 @@ namespace Exam_AnswerWeb.Controllers
                 ViewData["prev"] = intId - 1;
                 ViewData["next"] = intId + 1;
                 ViewData["current"] = intId;
-                ViewData["max"] = 51;
+                ViewData["max"] = 73;
             }
             QuestionViewModel questionViewModel = new QuestionViewModel()
             {
