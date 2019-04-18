fe7f1d7a670ea935f8e2950a1d11e68119dc44b7
Yordan Todorov <yordan.todorov@mentormate.bg>
2019-04-18 20:20:31 +0300

    all salesforce 98

--- Exam-answer/Exam-answerWeb/Controllers/SalesforceController.cs
+++ Exam-answer/Exam-answerWeb/Controllers/SalesforceController.cs
@@ -53,7 +53,7 @@ namespace Exam_AnswerWeb.Controllers
                 ViewData["prev"] = intId - 1;
                 ViewData["next"] = intId + 1;
                 ViewData["current"] = intId;
-                ViewData["max"] = 88;
+                ViewData["max"] = 98;
             }
             QuestionViewModel questionViewModel = new QuestionViewModel()
             {
