759fd845930abfe11c42c7dca4a2429a453cea4d
ytodorov <ytodorov@ytodorov.com>
2019-05-05 22:02:08 +0300

    m

---
+++ Exam-answer/Exam-answerWeb/Models/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam_answerWeb.Models
+{
+    public class BaseViewModel
+    {
+        public int Id { get; set; }
+
+        public int? Order { get; set; }
+
+        public long DateCreatedInMemory { get; set; }
+    }
+}
---
+++ Exam-answer/Exam-answerWeb/Models/ExamViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam_answerWeb.Models
+{
+    public class ExamViewModel : BaseViewModel
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Provider { get; set; }
+
+        public List<QuestionViewModel> Questions { get; set; }
+    }
+}
