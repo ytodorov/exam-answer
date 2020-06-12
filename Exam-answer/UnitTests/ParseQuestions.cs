84a743c32e4a1fe01e8ca55cd8a37f556ed90570
ytodorov <ytodorov@ytodorov.com>
2020-06-13 00:10:13 +0300

    new questions to 301, new sitemap

--- Exam-answer/UnitTests/ParseQuestions.cs
+++ Exam-answer/UnitTests/ParseQuestions.cs
@@ -2,6 +2,7 @@
 using Exam_answerWeb.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -67,6 +68,7 @@ namespace UnitTests
 </urlset>");
 
             var res = sb.ToString().ToLowerInvariant();
+            Debugger.Break();
         }
 
         [Fact]
