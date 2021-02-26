98413a4a40fb1a8f1e7dad776497bf0cdecb4591
ytodorov <ytodorov@ytodorov.com>
2021-02-27 00:26:46 +0200

    remove amazon. add urls to cutyt.com

--- Exam-answer/Exam-answerWeb/Controllers/AmazonSaac02Controller.cs
+++ Exam-answer/Exam-answerWeb/Controllers/AmazonSaac02Controller.cs
@@ -1,33 +1,33 @@
-﻿using AutoMapper;
-using DAL.Entities;
-using Exam_answerWeb.Controllers;
-using Exam_answerWeb.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿//using AutoMapper;
+//using DAL.Entities;
+//using Exam_answerWeb.Controllers;
+//using Exam_answerWeb.Models;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Filters;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Caching.Memory;
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
 
-namespace Exam_AnswerWeb.Controllers
-{
-    [Route("amazon/saa-c02")]
-    public class AmazonSaac02Controller : EaControllerBase
-    {
-        public AmazonSaac02Controller(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-        }
+//namespace Exam_AnswerWeb.Controllers
+//{
+//    [Route("amazon/saa-c02")]
+//    public class AmazonSaac02Controller : EaControllerBase
+//    {
+//        public AmazonSaac02Controller(IServiceProvider serviceProvider) : base(serviceProvider)
+//        {
+//        }
 
-        public IActionResult Index()
-        {
-            var examViewModel = GetExamViewModelFromHttpContext();
+//        public IActionResult Index()
+//        {
+//            var examViewModel = GetExamViewModelFromHttpContext();
 
-            return View($"_ExamPartial", examViewModel);
-        }
-    }
-}
+//            return View($"_ExamPartial", examViewModel);
+//        }
+//    }
+//}
